12c205708ceab4bee65cd5d3a7c42732aea5bd4b
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2026-03-25 21:35:27 +0200

    Change long line

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 class Car:
-    def __init__(self, comfort_class: str, clean_mark: str, brand: str):
+    def __init__(self, comfort_class: str, clean_mark: str, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: int, count_of_ratings: int):
+    def __init__(
+        self,
+        distance_from_city_center: int,
+        clean_power: int,
+        average_rating: int,
+        count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
